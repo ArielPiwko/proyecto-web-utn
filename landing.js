20745e87ae8eeba3bf584a734eaa48f02d4070b5
ArielPiwko <arielpiwko@gmail.com>
2022-09-30 22:18:04 -0300

    animacion esfumado

--- landing.js
+++ landing.js
@@ -1,12 +1,11 @@
 // attach to window.onload
 setTimeout(function(){
-
     document.getElementById('threeSeconds').className += ' hidden';
-    const el = document.getElementById('site-header');
-    el.classList.remove("site-header");
-
 }, 3000);
 
+setTimeout(function(){
+    document.getElementById('threeSeconds').className += ' nodisplay';
+}, 5000);
 
 setTimeout(function() {
     document.getElementById('logo').classList.toggle('rotated');
